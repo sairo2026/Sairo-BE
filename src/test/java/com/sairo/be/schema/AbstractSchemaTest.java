4b9a6d4b3c78b4f3a4b794ae9241f5eae9ae9df1
little stone <83966093+KimTaeHwan21@users.noreply.github.com>
2026-08-24 20:43:57 +0900

    feat: 카카오 전용 최종 스키마 Flyway V9 정합화 (#37)

--- src/test/java/com/sairo/be/schema/AbstractSchemaTest.java
+++ src/test/java/com/sairo/be/schema/AbstractSchemaTest.java
@@ -60,11 +60,12 @@ abstract class AbstractSchemaTest {
 
     protected long insertOffice(Connection conn, String name, String bizNo) throws SQLException {
         try (PreparedStatement ps = conn.prepareStatement(
-                "INSERT INTO office (name, representative_name, business_registration_number, phone, address_base) "
-                        + "VALUES (?, ?, ?, '010-0000-0000', '서울시') RETURNING id")) {
+                "INSERT INTO office (name, representative_name, business_registration_number, real_estate_license_number, phone, address_base) "
+                        + "VALUES (?, ?, ?, ?, '010-0000-0000', '서울시') RETURNING id")) {
             ps.setString(1, name);
             ps.setString(2, name);
             ps.setString(3, bizNo);
+            ps.setString(4, "LIC-" + UUID.randomUUID().toString().substring(0, 20));
             try (ResultSet rs = ps.executeQuery()) {
                 rs.next();
                 return rs.getLong(1);
@@ -74,8 +75,11 @@ abstract class AbstractSchemaTest {
 
     protected long insertApprovedSystemAdmin(Connection conn, long memberId, long officeId, String bizNo) throws SQLException {
         try (PreparedStatement ps = conn.prepareStatement(
-                "INSERT INTO office_registration (applicant_member_id, business_registration_number, status, resulting_office_id, reviewed_at) "
-                        + "VALUES (?, ?, 'APPROVED', ?, now())")) {
+                "INSERT INTO office_registration (applicant_member_id, business_registration_number, real_estate_license_number, "
+                        + "requested_office_name, requested_representative_name, requested_phone, requested_address_base, "
+                        + "requested_address_detail, status, resulting_office_id, reviewed_at) "
+                        + "SELECT ?, ?, real_estate_license_number, name, representative_name, phone, address_base, address_detail, "
+                        + "'APPROVED', id, now() FROM office WHERE id = ?")) {
             ps.setLong(1, memberId);
             ps.setString(2, bizNo);
             ps.setLong(3, officeId);
